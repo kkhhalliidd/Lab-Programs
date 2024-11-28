bba3f104d2009797410ee799e23d1275f1707d0c
kkhhalliidd <khalidrabbani.cs23@bmsce.ac.in>
2024-11-28 14:44:39 +0530

    Add files via upload
    source code for lab8

---
+++ lab.java
@@ -0,0 +1,37 @@
+class CollegeThread extends Thread {
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("BMS College of Engineering");
+                Thread.sleep(10000); 
+            }
+        } catch (InterruptedException e) {
+            System.out.println("CollegeThread interrupted: " + e.getMessage());
+        }
+    }
+}
+
+class DepartmentThread extends Thread {
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("CSE");
+                Thread.sleep(2000); 
+            }
+        } catch (InterruptedException e) {
+            System.out.println("DepartmentThread interrupted: " + e.getMessage());
+        }
+    }
+}
+
+public class lab {
+    public static void main(String[] args) {
+        CollegeThread collegeThread = new CollegeThread();
+        DepartmentThread departmentThread = new DepartmentThread();
+
+        collegeThread.start();
+        departmentThread.start();
+    }
+}
